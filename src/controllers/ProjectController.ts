6e78ffcfbfc3fb440bb42165e2dc215dab5b98ac
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-08 19:00:23 -0300

    logica de buscar todos los proyectos

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -18,6 +18,13 @@ export class ProjectController {
     }
 
     static getAllProjects = async (req: Request, res: Response) => {
-        res.send('Todos los projectos')
+
+        try {
+            const projects = await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
 }
