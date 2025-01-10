ae65e83a5715a5d70e2d5d3be0ee12f43b235efb
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-10 15:34:12 -0300

    aplicando cambios al modelo de tarea

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,10 +1,12 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
+import type { ITask } from "./Task";
 
 
 export interface IProject extends Document {
     projectName: string
     clientName: string
     description: string
+    tasks: PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
@@ -22,10 +24,16 @@ const ProjectSchema: Schema = new Schema({
         type: String,
         required: true,
         trim: true
-    }
+    },
+    tasks: [
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
 
 
-})
+},{timestamps: true})
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema)
 export default Project
