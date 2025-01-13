47ffb136719e164998f3e4572a340b5a3438fd5b
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-13 15:56:35 -0300

    Borrar tareas endpoint

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -79,5 +79,11 @@ router.put('/:projectId/tasks/:taskId',
     TaskController.updateTask
 )
 
+router.delete('/:projectId/tasks/:taskId',
+    param('taskId').isMongoId().withMessage('Id no valido'),
+    handleInputErrors,
+    TaskController.deleteTask
+)
+
 
 export default router
